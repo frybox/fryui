086f8e331886c6ae26c5dfb1a7d1d6e6371e53eb
juzejian <juzejian@gmail.com>
2023-12-23 16:20:16 +0000

    重新命名语义颜色，更新button定义

--- src/fryui/plugin.py
+++ src/fryui/plugin.py
@@ -7,14 +7,14 @@ radix_colored_metal = set(['gold', 'bronze'])
 
 radix_colored_colors = radix_colored_regular.union(radix_colored_bright, radix_colored_metal)
 
-radix_base_pure = set(['gray'])
-radix_base_desaturated = set(['mauve', 'slate', 'sage', 'olive', 'sand'])
+radix_gray_pure = set(['gray'])
+radix_gray_desaturated = set(['mauve', 'slate', 'sage', 'olive', 'sand'])
 
-radix_base_colors = radix_base_pure.union(radix_base_desaturated)
+radix_gray_colors = radix_gray_pure.union(radix_gray_desaturated)
 
-radix_colors = radix_colored_colors.union(radix_base_colors)
+radix_colors = radix_colored_colors.union(radix_gray_colors)
 
-def get_base_color(primary):
+def get_gray_color(primary):
     if primary in ('tomato', 'red', 'ruby', 'crimson', 'pink', 'plum', 'purple', 'violet',):
         return 'mauve'
     elif primary in ('iris', 'indigo', 'blue', 'sky', 'cyan',):
@@ -28,16 +28,16 @@ def get_base_color(primary):
     else:
         return 'gray'
 
-def get_content_color(color):
-    content_colors = {
+def get_contrast_color(color):
+    contrast_colors = {
         'sky': radix['slate-12'],
         'mint': radix['sage-12'],
         'lime': radix['olive-12'],
         'yellow': radix['sand-12'],
         'amber': radix['sand-12'],
     }
-    if color in content_colors:
-        return content_colors[color]
+    if color in contrast_colors:
+        return contrast_colors[color]
     if color in radix_colored_colors:
         return 'white'
     raise RuntimeError("Only support colored color step 9 content")
@@ -53,29 +53,40 @@ theme = {
     'error':     'tomato',
 }
 
+#radix-colors:
+# 1:  BackGround
+# 2:  BackGround eXtra
+# 3:  ELement background
+# 4:  ELement background eXtra
+# 5:  ELement background eXtra eXtra
+# 6:  BorDer
+# 7:  BorDer eXtra
+# 8:  BorDer eXtra eXtra
+# 9:  solid
+# 10: solid eXtra
+# 11: Text
+# 12: Text eXtra
+#              1       2        3       4        5         6       7        8        9    10     11     12
+suffixes  = ['-bg',  '-bgx',  '-el',  '-elx',  '-elxx',  '-bd',  '-bdx',  '-bdxx',  '',  'x',  '-t',  '-tx']
+asuffixes = ['-bga', '-bgax', '-ela', '-elax', '-elaxx', '-bda', '-bdax', '-bdaxx', 'a', 'ax', '-ta', '-tax']
 def base_css(theme=theme):
-    base, colored = check_theme(theme)
+    gray, colored = check_theme(theme)
     common_style = {}
     light_style = {
         'color-scheme': 'light',
-        '--base':         dump(radix[f'{base}-1']),
-        '--base-content': dump(radix[f'{base}-12']),
     }
     dark_style = {
         'color-scheme': 'dark',
-        '--base':         dump(radix[f'{base}-dark-1']),
-        '--base-content': dump(radix[f'{base}-dark-12']),
     }
     for name, value in colored.items():
-        common_style[f'--{name}'] = dump(radix[f'{value}-9'])
-        common_style[f'--{name}-content'] = dump(get_content_color(value))
-    allcolors = {'base':base, **colored}
+        common_style[f'--{name}-contrast'] = dump(get_contrast_color(value))
+    allcolors = {'gray':gray, **colored}
     for name, value in allcolors.items():
         for i in range(1, 13):
-            light_style[f'--{name}-{i}'] = dump(radix[f'{value}-{i}'])
-            light_style[f'--{name}-a{i}'] = dump(radix[f'{value}-a{i}'])
-            dark_style[f'--{name}-{i}'] = dump(radix[f'{value}-dark-{i}'])
-            dark_style[f'--{name}-a{i}'] = dump(radix[f'{value}-dark-a{i}'])
+            light_style[f'--{name}{suffixes[i-1]}'] = dump(radix[f'{value}-{i}'])
+            light_style[f'--{name}{asuffixes[i-1]}'] = dump(radix[f'{value}-a{i}'])
+            dark_style[f'--{name}{suffixes[i-1]}'] = dump(radix[f'{value}-dark-{i}'])
+            dark_style[f'--{name}{asuffixes[i-1]}'] = dump(radix[f'{value}-dark-a{i}'])
     return {
         ':root': common_style,
         ':root, [data-scheme="light"]': light_style,
@@ -87,21 +98,24 @@ def check_theme(theme):
     curr = dict(theme)
     if not all(n in semantic_names for n in curr.keys()):
         raise RuntimeError("theme should only have one of the 8 semantic names")
-    base = curr.pop('base', None)
+    gray = curr.pop('gray', None)
     if 'primary' not in curr:
         raise RuntimeError("'primary' should be in theme")
     if not all(v in radix_colored_colors for v in curr.values()):
-        raise RuntimeError("all semantic colors other than 'base' should be one of radix non-gray colors")
-    if not base:
-        base = get_base_color(curr['primary'])
-    elif base not in radix_base_colors:
-        raise RuntimeError("'base' color should be one of radix gray colors")
-    return base, curr
+        raise RuntimeError("all semantic colors other than 'gray' should be one of radix non-gray colors")
+    if not gray:
+        gray = get_gray_color(curr['primary'])
+    elif gray not in radix_gray_colors:
+        raise RuntimeError("'gray' color should be one of radix gray colors")
+    return gray, curr
 
-semantic_names = set(['primary', 'secondary', 'accent', 'base', 'info', 'success', 'warning', 'error'])
+semantic_colored_names = set(['primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error'])
+semantic_gray_name = 'gray'
+semantic_names = semantic_colored_names.union(set([semantic_gray_name]))
 
 def colors():
-    suffixes = ['', '-content'] + [f'-{i}' for i in range(1, 13)] + [f'-a{i}' for i in range(1, 13)]
-    return {f'{name}{suffix}': f'rgb(var(--{name}{suffix}))'
-            for suffix in suffixes
-            for name  in semantic_names}
+    cc = {f'{name}{suffix}': f'rgb(var(--{name}{suffix}))'
+          for suffix in (suffixes + asuffixes)
+          for name  in semantic_names}
+    cc.update({f'{name}-contrast': f'rgb(var(--{name}-contrast))' for name in semantic_colored_names})
+    return cc
